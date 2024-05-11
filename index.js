f2ad1a55e000659af3c513aabd6307fb611e9ea2
Awaken <matvey.zorka@gmail.com>
2024-05-12 02:58:23 +0300

    reload

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const token = process.env.DISCORD_TOKEN
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+client.cooldowns = new Collection();
 client.commands = new Collection();
+
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
